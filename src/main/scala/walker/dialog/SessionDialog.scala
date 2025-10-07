5ebcf5517c4b6f8c18395adee093363700f558a3
objektwerks <objektwerks@runbox.com>
2025-10-07 06:19:51 -0400

    edit

--- src/main/scala/walker/dialog/SessionDialog.scala
+++ src/main/scala/walker/dialog/SessionDialog.scala
@@ -6,12 +6,12 @@ import scalafx.scene.control.{ButtonType, ComboBox, Dialog}
 import scalafx.scene.control.ButtonBar.ButtonData
 import scalafx.scene.layout.Region
 
-import walker.{Client, Context, DistanceUnit, Entity, Session, WeightUnit}
+import walker.{App, Context, DistanceUnit, Entity, Session, WeightUnit}
 import walker.control.{CalorieTextField, DoubleTextField, IntTextField}
 import walker.control.DateTimeField
 
 final class SessionDialog(context: Context, session: Session) extends Dialog[Session]:
-  initOwner(Client.stage)
+  initOwner(App.stage)
   title = context.windowTitle
   headerText = context.dialogSession
 
