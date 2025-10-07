0dcc51d8f6f7cb0053904d4b6580e4f2edfd0d2e
objektwerks <objektwerks@runbox.com>
2025-10-07 05:36:00 -0400

    added ddl.sql, entity and store

---
+++ src/main/scala/walker/Entity.scala
@@ -0,0 +1,94 @@
+package walker
+
+import com.github.plokhotnyuk.jsoniter_scala.core.*
+import com.github.plokhotnyuk.jsoniter_scala.macros.*
+
+import java.time.{Instant, LocalDateTime, ZoneOffset}
+import java.time.format.DateTimeFormatter
+import java.util.UUID
+
+import scalafx.beans.property.ObjectProperty
+
+sealed trait Entity:
+  val id: Long
+
+object Entity:
+  given JsonValueCodec[Entity] = JsonCodecMaker.make[Entity](CodecMakerConfig.withDiscriminatorFieldName(None))
+
+  val formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd, hh:mm a")
+
+  def format(epochMillis: Long): String = formatter.format( toLocalDateTime(epochMillis) )
+  def format(localDateTime: LocalDateTime): String = formatter.format(localDateTime)
+
+  def toLocalDateTime(epochMillis: Long): LocalDateTime = LocalDateTime.ofInstant(Instant.ofEpochMilli(epochMillis), ZoneOffset.UTC)
+  def toEpochMillis(localDateTime: LocalDateTime): Long = localDateTime.toInstant(ZoneOffset.UTC).toEpochMilli
+
+final case class Account(id: Long = 0,
+                         license: String = UUID.randomUUID.toString,
+                         emailAddress: String = "",
+                         pin: String = Pin.newInstance,
+                         activated: Long = Instant.now.toEpochMilli,
+                         deactivated: Long = 0) extends Entity derives CanEqual
+
+object Account:
+  val empty = Account(license = "", emailAddress = "", pin = "", activated = 0, deactivated = 0)
+  given JsonValueCodec[Account] = JsonCodecMaker.make[Account]
+
+final case class Walker(id: Long = 0,
+                         accountId: Long,
+                         name: String) extends Entity derives CanEqual:
+  val nameProperty = ObjectProperty[String](this, "name", name)
+  val walker = this
+
+object Walker:
+  given JsonValueCodec[Walker] = JsonCodecMaker.make[Walker]
+  given walkerOrdering: Ordering[Walker] = Ordering.by[Walker, String](s => s.name)
+
+final case class Session(id: Long = 0,
+                         walkerId: Long,
+                         weight: Int = 150,
+                         weightUnit: String = WeightUnit.lb.toString,
+                         distance: Double = 2.0,
+                         distanceUnit: String = DistanceUnit.mi.toString,
+                         hours: Int = 0,
+                         minutes: Int = 40,
+                         calories: Int = 150,
+                         datetime: Long = Instant.now.toEpochMilli) extends Entity derives CanEqual:
+  val weightProperty = ObjectProperty[Double](this, "weight", weight)
+  val weightUnitProperty = ObjectProperty[String](this, "weightUnit", weightUnit)
+  val distanceProperty = ObjectProperty[Double](this, "distance", distance)
+  val distanceUnitProperty = ObjectProperty[String](this, "distanceUnit", distanceUnit)
+  val hoursProperty = ObjectProperty[Int](this, "hours", hours)
+  val minutesProperty = ObjectProperty[Int](this, "minutes", minutes)
+  val caloriesProperty = ObjectProperty[Int](this, "calories", calories)
+  val datetimeProperty = ObjectProperty[String](this, "datetime", Entity.format(datetime))
+  val session = this
+
+  def minutesWalked(): Int = (hours * 60) + minutes
+
+  def caloriesBurned(): Int =
+    if weight < 1.0 || minutes < 1 then 0
+    else
+      val kg = if WeightUnit.lb.toString == weightUnit then WeightUnit.lbsToKgs(weight) else weight.toDouble
+      val met = (Session.MET * 3.5 * kg) / 200
+      val mins = minutesWalked()
+      ( mins * met ).round.toInt
+
+object Session:
+  val MET = 6
+  given JsonValueCodec[Session] = JsonCodecMaker.make[Session]
+  given sessionOrdering: Ordering[Session] = Ordering.by[Session, Long](session => session.datetime).reverse
+
+enum WeightUnit:
+  case lb, kg
+
+object WeightUnit:
+  def lbsToKgs(lbs: Int): Double = lbs * 0.454
+  def kgsToLbs(kgs: Int): Double = kgs * 2.205
+  def toList: List[String] = WeightUnit.values.map(wu => wu.toString).toList
+
+enum DistanceUnit:
+  case km, mi
+
+object DistanceUnit:
+  def toList: List[String] = DistanceUnit.values.map(du => du.toString).toList
---
+++ src/main/scala/walker/Store.scala
@@ -0,0 +1,240 @@
+package walker
+
+import com.github.blemale.scaffeine.{Cache, Scaffeine}
+import com.typesafe.config.Config
+import com.zaxxer.hikari.HikariDataSource
+
+import java.time.LocalDate
+import java.util.concurrent.TimeUnit
+import javax.sql.DataSource
+
+import scalikejdbc.*
+import scala.concurrent.duration.FiniteDuration
+
+object Store:
+  def apply(config: Config) = new Store( cache(config), dataSource(config) )
+
+  def cache(config: Config): Cache[String, String] =
+    Scaffeine()
+      .initialCapacity(config.getInt("cache.initialSize"))
+      .maximumSize(config.getInt("cache.maxSize"))
+      .expireAfterWrite( FiniteDuration( config.getLong("cache.expireAfter"), TimeUnit.HOURS) )
+      .build[String, String]()
+
+  def dataSource(config: Config): DataSource =
+    val ds = HikariDataSource()
+    ds.setDataSourceClassName(config.getString("db.driver"))
+    ds.addDataSourceProperty("url", config.getString("db.url"))
+    ds.addDataSourceProperty("user", config.getString("db.user"))
+    ds.addDataSourceProperty("password", config.getString("db.password"))
+    ds
+
+final class Store(cache: Cache[String, String],
+                  dataSource: DataSource):
+  ConnectionPool.singleton( DataSourceConnectionPool(dataSource) )
+
+  def register(account: Account): Account = addAccount(account)
+
+  def login(email: String, pin: String): Option[Account] =
+    DB readOnly { implicit session =>
+      sql"select * from account where email_address = $email and pin = $pin"
+        .map(rs =>
+          Account(
+            rs.long("id"),
+            rs.string("license"),
+            rs.string("email_address"),
+            rs.string("pin"),
+            rs.long("activated"),
+            rs.long("deactivated")
+          )
+        )
+        .single()
+    }
+
+  def isEmailAddressUnique(emailAddress: String): Boolean =
+    val count = DB readOnly { implicit session =>
+      sql"select id from account where email_address = $emailAddress"
+        .map(rs => rs.long("id"))
+        .single()
+    }
+    if count.isDefined then false else true
+
+  def isAuthorized(license: String): Boolean =
+    cache.getIfPresent(license) match
+      case Some(_) => true
+      case None =>
+        val optionalLicense = DB readOnly { implicit session =>
+          sql"select license from account where license = $license"
+            .map(rs => rs.string("license"))
+            .single()
+        }
+        if optionalLicense.isDefined then
+          cache.put(license, license)
+          true
+        else false
+
+  def listAccounts(): List[Account] =
+    DB readOnly { implicit session =>
+      sql"select * from account"
+        .map(rs =>
+          Account(
+            rs.long("id"),
+            rs.string("license"),
+            rs.string("email_address"),
+            rs.string("pin"),
+            rs.long("activated"),
+            rs.long("deactivated")
+          )
+        )
+        .list()
+    }
+
+  def addAccount(account: Account): Account =
+    val id = DB localTx { implicit session =>
+      sql"""
+        insert into account(license, email_address, pin, activated, deactivated)
+        values(${account.license}, ${account.emailAddress}, ${account.pin}, ${account.activated}, ${account.deactivated})
+      """
+      .updateAndReturnGeneratedKey()
+    }
+    account.copy(id = id)
+
+  def removeAccount(license: String): Unit =
+    DB localTx { implicit session =>
+      sql"delete account where license = $license"
+      .update()
+    }
+    ()
+
+  def deactivateAccount(license: String): Option[Account] =
+    DB localTx { implicit session =>
+      val deactivated = sql"update account set deactivated = ${LocalDate.now.toEpochDay}, activated = 0 where license = $license"
+      .update()
+      if deactivated > 0 then
+        sql"select * from account where license = $license"
+          .map(rs =>
+            Account(
+              rs.long("id"),
+              rs.string("license"),
+              rs.string("email_address"),
+              rs.string("pin"),
+              rs.long("activated"),
+              rs.long("deactivated")
+            )
+          )
+          .single()
+      else None
+    }
+
+  def reactivateAccount(license: String): Option[Account] =
+    DB localTx { implicit session =>
+      val activated = sql"update account set activated = ${LocalDate.now.toEpochDay}, deactivated = 0 where license = $license"
+      .update()
+      if activated > 0 then
+        sql"select * from account where license = $license"
+          .map(rs =>
+            Account(
+              rs.long("id"),
+              rs.string("license"),
+              rs.string("email_address"),
+              rs.string("pin"),
+              rs.long("activated"),
+              rs.long("deactivated")
+            )
+          )
+          .single()
+      else None
+    }
+
+  def listWalkers(accountId: Long): List[Walker] =
+    DB readOnly { implicit session =>
+      sql"select * from walker where account_id = $accountId order by name"
+        .map(rs =>
+          Walker(
+            rs.long("id"),
+            rs.long("account_id"),
+            rs.string("name"), 
+          )
+        )
+        .list()
+    }
+
+  def addWalker(walker: Walker): Long =
+    DB localTx { implicit session =>
+      sql"""
+        insert into walker(account_id, name) values(${walker.accountId}, ${walker.name})
+        """
+        .updateAndReturnGeneratedKey()
+    }
+
+  def updateWalker(walker: Walker): Long =
+    DB localTx { implicit session =>
+      sql"""
+        update walker set name = ${walker.name}
+        where id = ${walker.id}
+        """
+        .update()
+      walker.id
+    }
+
+  def listSessions(walkerId: Long): List[Session] =
+    DB readOnly { implicit session =>
+      sql"select * from session where walker_id = $walkerId order by datetime desc"
+        .map(rs =>
+          Session(
+            rs.long("id"),
+            rs.long("walker_id"),
+            rs.int("weight"),
+            rs.string("weight_unit"),
+            rs.double("distance"),
+            rs.string("distance_unit"),
+            rs.int("hours"),
+            rs.int("minutes"),
+            rs.int("calories"),
+            rs.long("datetime")
+          )
+        )
+        .list()
+    }
+
+  def addSession(sess: Session): Long =
+    DB localTx { implicit session =>
+      sql"""
+        insert into session(walker_id, weight, weight_unit, distance, distance_unit, hours, minutes, calories, datetime)
+        values(${sess.walkerId}, ${sess.weight}, ${sess.weightUnit}, ${sess.distance}, ${sess.distanceUnit}, ${sess.hours},
+        ${sess.minutes}, ${sess.calories}, ${sess.datetime})
+        """
+        .updateAndReturnGeneratedKey()
+    }
+
+  def updateSession(sess: Session): Long =
+    DB localTx { implicit session =>
+      sql"""
+        update session set weight = ${sess.weight}, weight_unit = ${sess.weightUnit}, distance = ${sess.distance},
+        distance_unit = ${sess.distanceUnit}, hours = ${sess.hours}, minutes = ${sess.minutes}, calories = ${sess.calories},
+        datetime = ${sess.datetime} where id = ${sess.id}
+        """
+        .update()
+      sess.id
+    }
+
+  def listFaults(): List[Fault] =
+    DB readOnly { implicit session =>
+      sql"select * from fault order by occurred desc"
+        .map(rs =>
+          Fault(
+            rs.string("cause"),
+            rs.string("occurred")
+          )
+        )
+        .list()
+    }
+
+  def addFault(fault: Fault): Fault =
+    DB localTx { implicit session =>
+      sql"""
+        insert into fault(cause, occurred) values(${fault.cause}, ${fault.occurred})
+        """
+        .update()
+        fault
+    }
