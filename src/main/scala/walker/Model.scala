a2878f74edbf9941b21e1d6b4cc5385a02f1e396
objektwerks <objektwerks@runbox.com>
2025-10-07 06:12:30 -0400

    refactored add session in model

--- src/main/scala/walker/Model.scala
+++ src/main/scala/walker/Model.scala
@@ -58,18 +58,10 @@ final class Model(store: Store) extends LazyLogging:
   def add(session: Session)(runLast: => Unit): Unit =
     supervised:
       assertNotInFxThread(s"add session: $session")
-      fetcher.fetch(
-        SaveSession(objectAccount.get.license, session),
-        (event: Event) => event match
-          case fault @ Fault(_, _) => onFetchFault("add session", session, fault)
-          case SessionSaved(id) =>
-            observableSessions.insert(0, session.copy(id = id))
-            observableSessions.sort()
-            selectedSessionId.set(id)
-            logger.info(s"Added session: $session")
-            runLast
-          case _ => ()
-      )
+      val id = store.addSession(session)
+      observableSessions.insert(0, session.copy(id = id))
+      selectedSessionId.value = id
+      logger.info(s"Added session: $session")
 
   def update(selectedIndex: Int, session: Session)(runLast: => Unit): Unit =
     supervised:
